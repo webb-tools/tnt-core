6b4e5590975fca0010c01ae53f8fa8f552160fb0
Shady Khalifa <dev+github@shadykhalifa.me>
2024-08-16 19:39:57 +0300

    Improve docs

--- src/BlueprintServiceManager.sol
+++ src/BlueprintServiceManager.sol
@@ -3,10 +3,31 @@ pragma solidity ^0.8.19;
 
 import "core/Permissions.sol";
 
-/// @dev Created by the service blueprint designer (gadget developer)
+/**
+ * @title BlueprintServiceManager
+ * @dev This contract acts as a manager for the lifecycle of a Blueprint Instance,
+ * facilitating various stages such as registration, service requests, job execution,
+ * and job result handling. It is designed to be used by the service blueprint designer
+ * (gadget developer) and integrates with the RootChain for permissioned operations.
+ * Each function serves as a hook for different lifecycle events, and reverting any
+ * of these functions interrupts the process flow.
+ */
 contract BlueprintServiceManager is RootChainEnabled {
+    /**
+     * @dev Hook for service operator registration. Called when a service operator
+     * attempts to register with the blueprint.
+     * @param operator The operator's details.
+     * @param registrationInputs Inputs required for registration.
+     */
     function onRegister(bytes calldata operator, bytes calldata registrationInputs) public payable virtual onlyFromRootChain { }
 
+    /**
+     * @dev Hook for service instance requests. Called when a user requests a service
+     * instance from the blueprint.
+     * @param serviceId The ID of the requested service.
+     * @param operators The operators involved in the service.
+     * @param requestInputs Inputs required for the service request.
+     */
     function onRequest(
         uint64 serviceId,
         bytes[] calldata operators,
@@ -18,6 +39,14 @@ contract BlueprintServiceManager is RootChainEnabled {
         onlyFromRootChain
     { }
 
+    /**
+     * @dev Hook for job calls on the service. Called when a job is executed within
+     * the service context.
+     * @param serviceId The ID of the service where the job is called.
+     * @param job The job identifier.
+     * @param jobCallId A unique ID for the job call.
+     * @param inputs Inputs required for the job execution.
+     */
     function onJobCall(
         uint64 serviceId,
         uint8 job,
@@ -30,6 +59,16 @@ contract BlueprintServiceManager is RootChainEnabled {
         onlyFromRootChain
     { }
 
+    /**
+     * @dev Hook for handling job call results. Called when operators send the result
+     * of a job execution.
+     * @param serviceId The ID of the service related to the job.
+     * @param job The job identifier.
+     * @param jobCallId The unique ID for the job call.
+     * @param participant The participant (operator) sending the result.
+     * @param inputs Inputs used for the job execution.
+     * @param outputs Outputs resulting from the job execution.
+     */
     function onJobCallResult(
         uint64 serviceId,
         uint8 job,
@@ -43,6 +82,18 @@ contract BlueprintServiceManager is RootChainEnabled {
         onlyFromRootChain
     { }
 
+    /**
+     * @dev Verifies the result of a job call. This function is used to validate the
+     * outputs of a job execution against the expected results.
+     * @param serviceId The ID of the service related to the job.
+     * @param job The job identifier.
+     * @param jobCallId The unique ID for the job call.
+     * @param participant The participant (operator) whose result is being verified.
+     * @param inputs Inputs used for the job execution.
+     * @param outputs Outputs resulting from the job execution.
+     * @return bool Returns true if the job call result is verified successfully,
+     * otherwise false.
+     */
     function verifyJobCallResult(
         uint64 serviceId,
         uint8 job,
@@ -56,7 +107,5 @@ contract BlueprintServiceManager is RootChainEnabled {
         virtual
         onlyFromRootChain
         returns (bool)
-    {
-        return true;
-    }
+    { }
 }
--- src/Permissions.sol
+++ src/Permissions.sol
@@ -18,6 +18,8 @@ contract RootChainEnabled {
     /// @dev address(keccak256(pallet_services::Config::PalletId::to_account_id())[0:20])
     address public constant ROOT_CHAIN = 0x6d6f646c70792F73727663730000000000000000;
 
+    /// @dev Only root chain can call this function
+    /// @notice This function can only be called by the root chain
     modifier onlyFromRootChain() {
         require(msg.sender == ROOT_CHAIN, "RootChain: Only root chain can call this function");
         _;
