fad7030fe53ea5d4a7576316880f5416fe656ee9
drewstone <drewstone329@gmail.com>
2024-09-12 16:11:35 +0900

    Delete all liquifier work and move to new repo (#11)

--- src/lst/utils/ERC721Receiver.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.8.19;
-
-abstract contract ERC721Receiver {
-    function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
-        return ERC721Receiver.onERC721Received.selector;
-    }
-}
